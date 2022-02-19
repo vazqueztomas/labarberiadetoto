cecb11bdb1fb0aa681ab00bdfe7b85ba0257f842
Tomás Vazquez <78186062+vazquezcabj21@users.noreply.github.com>
2022-02-19 20:44:49 -0300

    add fixes, bootstrap

---
+++ public/script/main.js
@@ -0,0 +1,16 @@
+const d = document;
+const sectionFotos = d.getElementById('muestra-fotos ')
+
+
+// si detecta que esta en mobile
+if( navigator.userAgent.match(/Android/i)
+|| navigator.userAgent.match(/webOS/i)
+|| navigator.userAgent.match(/iPhone/i)
+|| navigator.userAgent.match(/iPad/i)
+|| navigator.userAgent.match(/iPod/i)
+|| navigator.userAgent.match(/BlackBerry/i)
+|| navigator.userAgent.match(/Windows Phone/i)) {
+    sectionFotos.classList.add('oculto')
+} else {
+    sectionFotos.classList.remove('oculto');
+}
